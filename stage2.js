7b15b405b0ca0f1f50c8c8edc3327634c15d6126
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2022-10-01 13:52:26 -0700

    2-3

--- stage2.js
+++ stage2.js
@@ -1,12 +1,14 @@
+function N (){
+	alert("I'm sorry, but there is no place for liars on this website ;)")
+	N()
+}
+
 function startStage2 (){
 var start = prompt("Were you redirected here? Y/N")
 if(start == "Y"){
-	
+
 	}else if(start =="N"){
-		var urmom = 0
-		while(urmom = 0){
-			alert("I'm sorry, but there is no place for liars in this puzzle ;)")
-		}
+		N()
 	}else{
 		startStage2()
 	}
