cbbe57587771b33511d68324ffaaf216b015ec66
Owenthecoder1 <71456832+Owenthecoder1@users.noreply.github.com>
2022-10-01 19:14:49 -0700

    testing 2

--- stage3.js
+++ stage3.js
@@ -44,5 +44,5 @@ function win (){
 
 nameVerify.addEventListener("click", nameVerifyRun)
 var urmomjustwon = document.getElementById("urmomjustwon")
-console.log urmomjustwon
+console.log(urmomjustwon)
 urmomjustwon.addEventListener("click",win)
